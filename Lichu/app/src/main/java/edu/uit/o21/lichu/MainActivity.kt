058c95029397bfd9d050cb358baaae418a333dd8
uynNee <uynnee1812@gmail.com>
2024-05-09 11:29:54 +0700

    Update UI changes

--- Lichu/app/src/main/java/edu/uit/o21/lichu/MainActivity.kt
+++ Lichu/app/src/main/java/edu/uit/o21/lichu/MainActivity.kt
@@ -12,7 +12,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
@@ -22,6 +21,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
@@ -34,7 +34,6 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             LichuTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
@@ -46,64 +45,67 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-data class CategoryColors(val background: Color, val note: Color)
-
-val category1Colors = CategoryColors(Color(0xFFD8A6FA), Color(0xFFDCD7FF))
-val category2Colors = CategoryColors(Color(0xFFBBA6FA), Color(0xFFE0D7FF))
-val category3Colors = CategoryColors(Color(0xFFA6B3FA), Color(0xFFD7DEFF))
-
+sealed class CategoryColors(val backgroundColor: Color, val noteColor: Color) {
+    data object Category1 : CategoryColors(Color(0xFFD8A6FA), Color(0xFFF0D3FD))
+    data object Category2 : CategoryColors(Color(0xFFBBA6FA), Color(0xFFE5D7FF))
+    data object Category3 : CategoryColors(Color(0xFFA6B3FA), Color(0xFFD7D6FF))
+}
 
 @Composable
 fun MainScreen(modifier: Modifier = Modifier) {
-    val selectedItem = remember { mutableIntStateOf(0) }
-
+    val selectedTabIndex = remember { mutableIntStateOf(0) }
     Column(
         modifier = modifier.fillMaxSize(),
         verticalArrangement = Arrangement.SpaceBetween
     ) {
-        Column {
-            Category("Category 1", category1Colors) {
-                Note("Note 1.1", "Due Date 1.1", category1Colors)
-                Note("Note 1.2", "Due Date 1.2", category1Colors)
-                Note("Note 1.3", "Due Date 1.3", category1Colors)
-            }
-            Category("Category 2", category2Colors) {
-                Note("Note 2.1", "Due Date 2.1", color = category2Colors)
-                Note("Note 2.2", "Due Date 2.2", color = category2Colors)
-                Note("Note 2.3", "Due Date 2.3", color = category2Colors)
-            }
-            Category("Category 3", category3Colors) {
-                Note("Note 3.1", "Due Date 3.1", color = category3Colors)
-                Note("Note 3.2", "Due Date 3.2", color = category3Colors)
-            }
-        }
-        BottomNavBar(selectedItem.intValue) { newIndex ->
-            selectedItem.intValue = newIndex
+        CategoryList()
+        BottomNavBar(selectedTabIndex.intValue) { newIndex ->
+            selectedTabIndex.intValue = newIndex
             // Handle navigation here
         }
     }
 }
 
 @Composable
+fun CategoryList() {
+    Column {
+        Text("Notes", style = MaterialTheme.typography.titleLarge)
+        Category("Category 1", CategoryColors.Category1) {
+            Note("Note 1.1", "Due Date 1.1", CategoryColors.Category1)
+            Note("Note 1.2", "Due Date 1.2", CategoryColors.Category1)
+            Note("Note 1.3", "Due Date 1.3", CategoryColors.Category1)
+        }
+        Category("Category 2", CategoryColors.Category2) {
+            Note("Note 2.1", "Due Date 2.1", CategoryColors.Category2)
+            Note("Note 2.2", "Due Date 2.2", CategoryColors.Category2)
+            Note("Note 2.3", "Due Date 2.3", CategoryColors.Category2)
+        }
+        Category("Category 3", CategoryColors.Category3) {
+            Note("Note 3.1", "Due Date 3.1", CategoryColors.Category3)
+            Note("Note 3.2", "Due Date 3.2", CategoryColors.Category3)
+        }
+    }
+}
+
+@Composable
 fun BottomNavBar(
-    selectedItem: Int,
+    selectedTabIndex: Int,
     onItemClick: (Int) -> Unit
 ) {
     NavigationBar(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(16.dp),
     ) {
         NavigationBarItem(
-            icon = { Icon(painter = painterResource(id = android.R.drawable.ic_menu_view), contentDescription = "Notes") },
+            icon = { Icon(painter = painterResource(id = android.R.drawable.ic_menu_gallery), contentDescription = "Notes") },
             label = { Text("Notes") },
-            selected = selectedItem == 0,
+            selected = selectedTabIndex == 0,
             onClick = { onItemClick(0) }
         )
         NavigationBarItem(
             icon = { Icon(painter = painterResource(id = android.R.drawable.ic_menu_month), contentDescription = "Calendar") },
             label = { Text("Calendar") },
-            selected = selectedItem == 1,
+            selected = selectedTabIndex == 1,
             onClick = { onItemClick(1) }
         )
     }
@@ -114,7 +116,8 @@ fun Category(header: String, colors: CategoryColors, notes: @Composable () -> Un
     Column(
         modifier = Modifier
             .padding(16.dp)
-            .background(colors.background)
+            .fillMaxWidth()
+            .background(colors.backgroundColor)
     ) {
         Text(header, style = MaterialTheme.typography.titleLarge)
         Spacer(modifier = Modifier.height(8.dp))
@@ -123,25 +126,37 @@ fun Category(header: String, colors: CategoryColors, notes: @Composable () -> Un
 }
 
 @Composable
-fun Note(header: String, dueDate: String, color: CategoryColors) {
+fun Note(header: String, dueDate: String, colors: CategoryColors) {
     Column(
         modifier = Modifier
-            .padding(8.dp)
-            .background(color.note)
+            .padding(6.dp)
+            .fillMaxWidth()
+            .background(colors.noteColor)
     ) {
-        Row {
-            Text(header, style = MaterialTheme.typography.titleSmall)
-            Spacer(modifier = Modifier.width(8.dp))
-            Text(dueDate, style = MaterialTheme.typography.bodySmall)
+        Spacer(modifier = Modifier.height(4.dp))
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                header,
+                style = MaterialTheme.typography.titleSmall,
+                modifier = Modifier.weight(1f).padding(start = 12.dp)
+            )
+            Text(
+                dueDate,
+                style = MaterialTheme.typography.bodySmall,
+                modifier = Modifier.padding(end = 8.dp)
+            )
         }
         Spacer(modifier = Modifier.height(4.dp))
-        // Add more details or actions for the note
     }
 }
 
-@Preview(showBackground= true)
+@Preview(showBackground = true)
 @Composable
-fun GreetingPreview(){
+fun MainScreenPreview() {
     LichuTheme {
         MainScreen()
     }
