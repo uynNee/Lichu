bdf55d89af984c0615416d56754de9386e2b57b5
uynNee <uynnee1812@gmail.com>
2024-06-18 16:11:25 +0700

    Init

--- Lichu/app/src/main/java/edu/uit/o21/lichu/MainActivity.kt
+++ Lichu/app/src/main/java/edu/uit/o21/lichu/MainActivity.kt
@@ -13,28 +13,5 @@ import androidx.lifecycle.ViewModelProvider
 
 
 class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        val viewModel = ViewModelProvider(this, ToDoViewModelFactory(application))[ToDoViewModel::class.java]
-        setContent {
-            LichuTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    ToDoScreen(viewModel)
-                }
-            }
-        }
-    }
-}
 
-class ToDoViewModelFactory(private val application: Application) : ViewModelProvider.Factory {
-    override fun <T : ViewModel> create(modelClass: Class<T>): T {
-        if (modelClass.isAssignableFrom(ToDoViewModel::class.java)) {
-            @Suppress("UNCHECKED_CAST")
-            return ToDoViewModel(application) as T
-        }
-        throw IllegalArgumentException("Unknown ViewModel class")
-    }
 }
